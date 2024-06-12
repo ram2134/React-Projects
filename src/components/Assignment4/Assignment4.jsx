6509967d6e21352785ede4421b245089251cc5d5
Ramachandra <amnvramachandra213@gmail.com>
2024-06-12 16:59:45 +0530

    assignment4 modification

--- src/components/Assignment4/Assignment4.jsx
+++ src/components/Assignment4/Assignment4.jsx
@@ -5,7 +5,8 @@ import "./Assignment4.css";
 function Assignment4() {
   const [Count, setCount] = useState();
   const [Para, setPara] = useState("");
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const words = faker.lorem.words(Count);
     setPara(words);
   };
@@ -13,14 +14,17 @@ function Assignment4() {
     <div>
       <div className="Title">Paragraph Generator</div>
       <div className="body">
-        <input
-          type="text"
-          value={Count}
-          placeholder="enter Count of words"
-          onChange={(e) => setCount(Number(e.target.value))}
-        ></input>
-        <button onClick={handleSubmit}>Submit</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={Count}
+            placeholder="enter Count of words"
+            onChange={(e) => setCount(Number(e.target.value))}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
       </div>
+
       <div className="Paragraph-body">{Para}</div>
     </div>
   );
