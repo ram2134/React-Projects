9a058aa88b181face104ed5be6c27dd45b12406d
Ramachandra <amnvramachandra213@gmail.com>
2024-06-12 14:35:02 +0530

    assignment 4 ready

--- src/components/Assignment4/Assignment4.jsx
+++ src/components/Assignment4/Assignment4.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { useState } from "react";
-
+import { faker } from "@faker-js/faker";
+import "./Assignment4.css";
 function Assignment4() {
-  const [Number, setNumber] = useState();
+  const [Count, setCount] = useState();
   const [Para, setPara] = useState("");
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    const words = faker.lorem.words(Count);
+    setPara(words);
+  };
   return (
     <div>
       <div className="Title">Paragraph Generator</div>
       <div className="body">
         <input
           type="text"
-          value={Number}
-          placeholder="enter number of words"
-          onChange={(e) => setNumber(e.target.value)}
+          value={Count}
+          placeholder="enter Count of words"
+          onChange={(e) => setCount(Number(e.target.value))}
         ></input>
         <button onClick={handleSubmit}>Submit</button>
       </div>
-      <div>{Para}</div>
+      <div className="Paragraph-body">{Para}</div>
     </div>
   );
 }
